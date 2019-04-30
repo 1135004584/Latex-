0937ceccea3a8dec07d74b8e136ecfba065e26f7
bool <1135004584@qq.com>
2019-04-30 14:58:24 +0800

    init

---
+++ auto/test.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "test"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("ctexart" "UTF8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "ctexart"
+    "ctexart10")))
+
